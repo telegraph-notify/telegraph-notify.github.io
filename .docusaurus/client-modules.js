046c7d56270f6a57c89b734c74ee391da6aa8afc
Fran G <frances.h.gray@gmail.com>
2025-01-30 14:50:30 -0800

    Revert "fix: updating images"
    This reverts commit f829cee1202ec6ae1d9113e29f64bc91baf81f75.

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/frances/Capstone_Project/Telegraph-Website/telegraph-notify.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/frances/Capstone_Project/Telegraph-Website/telegraph-notify.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/frances/Capstone_Project/Telegraph-Website/telegraph-notify.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/frances/Capstone_Project/Telegraph-Website/telegraph-notify.github.io/src/css/custom.css"),
-];
